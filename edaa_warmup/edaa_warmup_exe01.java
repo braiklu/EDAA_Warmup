28f9bc06873140a39d95b02939865c50d7782bfd
Lucas <89614811+braiklu@users.noreply.github.com>
2022-09-05 19:51:52 -0300

    Add files via upload

---
+++ edaa_warmup/edaa_warmup_exe01.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.edaa_warmup;
+
+/**
+ *
+ * @author lucas braich
+ * 
+ *  Welison Pereira de Paula Gomes - R.A 12111604
+    Lucas Braich Ferreira - R.A 121115890
+ * 
+ * 
+ */
+public class edaa_warmup_exe01 {
+
+    public static void main(String[] args) {
+        int tabuada[][] = new int[10][10];
+
+            for (int line = 0; line < 10; line++) {
+                for (int column = 0; column < 10; column++) {
+
+                    int num = line + column;
+                    tabuada[line][column] = num;
+                    System.out.print(tabuada[line][column] + "\t | ");
+                }
+                System.out.println("");
+            }
+    }
+}
+
+
